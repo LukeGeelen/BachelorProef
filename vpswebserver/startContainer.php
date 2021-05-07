574788a323c3c3aa5579cd795a30c00283b2a80d
LukeGeelen <luke.geelen@student.uhasselt.be>
2021-05-07 13:54:15 +0200

    added java support

--- vpswebserver/startContainer.php
+++ vpswebserver/startContainer.php
@@ -1,22 +1,23 @@
 <?php
 //parse_str(implode('&', array_slice($argv, 1)), $_GET);
-if($_POST["lang"] != "C"){
-	die("not supported language");
-}
- foreach ($_FILES["file"]["error"] as $key => $error) {
+if($_POST["lang"] == "C"){
+//	die("not supported language");
+
+foreach ($_FILES["file"]["error"] as $key => $error) {
             if ($error == UPLOAD_ERR_OK) {
                 $tmp_name = $_FILES["file"]["tmp_name"][$key];
                 // basename() may prevent filesystem traversal attacks;
                 // further validation/sanitation of the filename may be appropriate
                 $name = basename($_FILES["file"]["name"][$key]);
-                move_uploaded_file($tmp_name, '/var/www/html/docker/code' . '/'. $name);
+                move_uploaded_file($tmp_name, '/var/www/html/dockerC/code' . '/'. $name);
             }
 }
 echo "starting";
-$build = shell_exec("docker build -t student_execution /var/www/html/docker/ --build-arg CACHEBUST=$(date +%s)");
-
+$build = shell_exec("docker build -t student_execution /var/www/html/dockerC/ --build-arg CACHEBUST=$(date +%s)");
+echo $build;
 echo "build done, running";
-$run =  shell_exec("docker run --net host -t student_execution" .  "> /dev/null 2>/dev/null &");
+$run =  shell_exec("docker run --net host -t student_execution"); //.  "> /dev/null 2>/dev/null &");
+echo $run;
 echo "done";
 
 $files = glob('/var/www/html/docker/code/*'); // get all file names
@@ -26,4 +27,33 @@ foreach($files as $file){ // iterate files
   }
 }
 echo "finished cleanup";
+}elseif($_POST["lang"] == "java"){
+foreach ($_FILES["file"]["error"] as $key => $error) {
+            if ($error == UPLOAD_ERR_OK) {
+                $tmp_name = $_FILES["file"]["tmp_name"][$key];
+                // basename() may prevent filesystem traversal attacks;
+                // further validation/sanitation of the filename may be appropriate
+                $name = basename($_FILES["file"]["name"][$key]);
+                move_uploaded_file($tmp_name, '/var/www/html/dockerJava/code' . '/'. $name);
+            }
+}
+
+echo "starting";
+$build = shell_exec("docker build -t student_execution /var/www/html/dockerJava/ --build-arg CACHEBUST=$(date +%s)");
+echo $build;
+echo "build done, running";
+$run =  shell_exec("docker run --net host -t student_execution"); //  "> /dev/null 2>/dev/null &");
+echo $run;
+echo "done";
+
+$files = glob('/var/www/html/dockerJava/code/*'); // get all file names
+foreach($files as $file){ // iterate files
+  if(is_file($file)) {
+//    unlink($file); // delete file
+  }
+}
+}else{
+    die("language not supported");
+}
+
 ?>
