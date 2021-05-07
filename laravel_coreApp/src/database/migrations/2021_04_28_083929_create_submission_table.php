17d3dd9c6f815b4f980f8d814f713a643033ee26
LukeGeelen <luke.geelen@student.uhasselt.be>
2021-05-07 14:31:59 +0200

    added database field for compiler and linter output

--- laravel_coreApp/src/database/migrations/2021_04_28_083929_create_submission_table.php
+++ laravel_coreApp/src/database/migrations/2021_04_28_083929_create_submission_table.php
@@ -18,6 +18,8 @@ class CreateSubmissionTable extends Migration
             $table->foreignId("assignment_id");
             $table->string('resource_path')->nullable();
             $table->string('name');
+            $table->string('compiler')->nullable();
+            $table->string('linter')->nullable();
             $table->timestamps();
         });
     }
